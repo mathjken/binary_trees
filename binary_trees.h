41ede3e88bc35610dafda0555e6461668bde7810
Ogu Johnkennedy <ogujohnkennedy@gmail.com>
2022-11-17 15:00:40 -0800

    new corrections

--- binary_trees.h
+++ binary_trees.h
@@ -68,4 +68,5 @@ heap_t *heap_insert(heap_t **root, int value);
 heap_t *array_to_heap(int *array, size_t size);
 int heap_extract(heap_t **root);
 int *heap_to_sorted_array(heap_t *heap, size_t *size);
+int greater_than(const binary_tree_t *tree, int val)
 #endif
